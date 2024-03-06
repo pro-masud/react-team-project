d52c390bd6ef0041a1679b1e2ae30df6183b07c9
pro-masud <promasudbd@gmail.com>
2024-03-06 23:56:42 +0600

    create a team member conponents today

---
+++ src/components/team/team.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Team = () => {
+  return (
+    <>
+      <div className="teamMember">
+        <div className="singleMember">
+            <div className="userImage">
+                <img src="" alt="" />
+            </div>
+            <h2>Masud rana</h2>
+            <p>PHP Developer</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Team;
---
+++ src/faker/data.js
@@ -0,0 +1,20 @@
+export const team = [
+    {
+        id: 1,
+        name: "Masud rana",
+        skill: "PHP Developer",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0k2tvp6R2BSwZgD77-8A12ETgtjS1uuzaFw&usqp=CAU"
+    },
+    {
+        id: 2,
+        name: "Firoj Hasan",
+        skill: "MERN Developer",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0k2tvp6R2BSwZgD77-8A12ETgtjS1uuzaFw&usqp=CAU"
+    },
+    {
+        id: 3,
+        name: "Nasim Islam",
+        skill: "React Developer",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0k2tvp6R2BSwZgD77-8A12ETgtjS1uuzaFw&usqp=CAU"
+    },
+]
